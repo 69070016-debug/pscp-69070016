0fd54a3c51d663a8357ec66e2a7533cf1e0b4aa0
69070016-debug <69070016@kmitl.ac.th>
2026-07-22 02:34:22 +0700

    Delete week2 directory

--- week2/Calculator.py
+++
@@ -1,9 +0,0 @@
-"""find how many time we need to click on calculator"""
-n = int(input())
-if n == 1:
-    print(1)
-else:
-    total_digits = 0
-    for i in range(1, n + 1):
-        total_digits += len(str(i))
-    print(total_digits + n)
--- week2/Gift I.py
+++
@@ -1,8 +0,0 @@
-"""gift"""
-def main():
-    """gift"""
-    m = float(input())
-    k = float(input())
-    a = (m*2) - k
-    print(a)
-main()
